504980690f4b4438e9409d526714e4a5492b51bf
José Valim <jose.valim@plataformatec.com.br>
2018-01-09 21:55:36 +0100

    Reproduce error locally

--- too_much.exs
+++ too_much.exs
@@ -1,20 +1,14 @@
 for _ <- 1..20 do
   spawn(fn ->
-    Process.flag(:trap_exit, true)
+    for i <- Stream.cycle([1, 2, 3]) do
+      Process.sleep(1)
 
-    [1, 2, 3]
-    |> Stream.cycle()
-    |> Task.async_stream(
-      fn i ->
+      spawn(fn ->
         for _ <- 1..i do
           require Logger
           Logger.error(List.duplicate(String.duplicate("oops ", 4), 10))
         end
-      end,
-      max_concurrency: 1000,
-      ordered: false,
-      timeout: 5000
-    )
-    |> Stream.run()
+      end)
+    end
   end)
 end
